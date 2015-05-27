8d591d2b52fee5ea0ab6ff0ec308830ce27e809b
Christopher Patton <chrispatton@gmail.com>
2015-05-27 13:11:56 -0700

    Ran beacon data with signal filter.

--- validate.py
+++ validate.py
@@ -62,10 +62,12 @@ def process(sv, sites, center, params):
   C = {} # site_id's --> confidence region estimate
   for t in np.arange(t_start, t_end+t_chunk, t_chunk):
       
-    print "chunk", ct; ct+=1
 
     # Signal data
-    sig = signal.Signal(db_con, dep_id, t, t+t_chunk, include=include, exclude=exclude)
+    sig = signal.Signal(db_con, dep_id, t, t+t_chunk, include=include, exclude=exclude,
+                          score_threshold=0.5)
+    print "chunk", ct, '(%d pulses)' % sig.get_count()
+    ct += 1
     if sig.t_start == float("+inf"):
       continue
 
@@ -126,12 +128,16 @@ def plot_map(p_known, sites, fn):
   
 
 def mean(P, site_ids): 
-  pos = np.array(P[site_ids])
+  pos = np.array(filter(lambda x : x != None, P[site_ids]))
   pos = pos[~np.isnan(pos)]
-  return np.mean(pos)
+  if len(pos) > 0: 
+    return np.mean(pos)
+  else:
+    return None
 
 def plot(P, fn, p_known=None):
   for site_ids in P.keys():
+    p_mean = mean(P, site_ids)
     if len(site_ids) > 1:
       pos = np.array(filter(lambda p: p!=None, P[site_ids]))
       #pos = pos[np.abs(pos - p_known) < 200]
@@ -141,7 +147,10 @@ def plot(P, fn, p_known=None):
       ax.set_xlabel('easting (m)')
       ax.set_ylabel('northing (m)')
       pp.scatter(np.imag(pos), np.real(pos), alpha=0.3, facecolors='b', edgecolors='none', s=25)#, zorder=11)
-      pp.plot(p_known.imag, p_known.real, color='w', marker='o', ms=7)
+      if p_known is not None: 
+        pp.plot(p_known.imag, p_known.real, color='w', marker='o', ms=7)
+      else: 
+        pp.plot(p_mean.imag, p_mean.real, color='w', marker='o', ms=7)
       pp.grid()
       pp.title("sites=%s total estimates=%d" % (str(site_ids), len(pos)))
       pp.tight_layout()
@@ -151,15 +160,14 @@ def plot(P, fn, p_known=None):
 def count(C): 
   res = {}
   for site_ids in C.keys():
-    if len(site_ids) > 1:
-      res[site_ids] = len(filter(lambda ell: ell!=None, C[site_ids]))
+    res[site_ids] = len(filter(lambda ell: ell!=None, C[site_ids]))
   return res
 
 def correlation(P, C, p_known=None): 
   res = {}
   for site_ids in P.keys():
     val = []; dist = []
-    if len(site_ids) > 1: 
+    if len(site_ids) > 1 and len(P[site_ids]) >= 10:
       p_mean = mean(P, site_ids)
       for i in range(len(P[site_ids])): 
         if C[site_ids][i] is not None and C[site_ids][i][1] > 0 and C[site_ids][i][2]> 0: 
@@ -173,13 +181,14 @@ def correlation(P, C, p_known=None):
       # First value is the correlation, the second is the p-value 
       # (probability of data asuuming they are uncorrelated)
       res[site_ids] = scipy.stats.stats.pearsonr(val, dist)
+    else: res[site_ids] = None
   return res
 
 def coverage(P, C, p_known=None): 
   res = {}
   for site_ids in P.keys():
     val = []
-    if len(site_ids) > 1: 
+    if len(site_ids) > 1:
       p_mean = mean(P, site_ids)
       for i in range(len(P[site_ids])): 
         if C[site_ids][i] is not None and C[site_ids][i][1] > 0 and C[site_ids][i][2]> 0: 
@@ -188,7 +197,11 @@ def coverage(P, C, p_known=None):
           if p_known is None:
             val.append(p_mean in E)
           else: val.append(p_known in E)
-      res[site_ids] = float(sum(val)) / len(val)
+      if len(val) > 0: 
+        res[site_ids] = (sum(val), len(val))
+      else: 
+        res[site_ids] = None
+    else: res[site_ids] = None
   return res
 
 
@@ -205,7 +218,7 @@ if __name__ == '__main__':
   (center, zone) = util.get_center(db_con)
   
   # Becaon parameters
-  params = { 't_step' : 120, 
+  params = { 't_step' : 15, 
              't_win' : 15, 
              't_chunk' : 3600 / 4,
              'dep_id' : 60,
@@ -219,7 +232,7 @@ if __name__ == '__main__':
   #t_start = 1396725598.548015
   #t_end = 1396732325.777558
 
-  prefix = 'beacon'
+  prefix = 'f0.5beacon' 
   fn = prefix + ''.join(map(lambda id: str(id), params['include']))
   print fn
      
@@ -228,20 +241,34 @@ if __name__ == '__main__':
   
   #P, C = process(sv, sites, center, params)
   #pickle.dump((P, C), open(fn+'.'+suffix, 'w'))
-#  (P, C) = pickle.load(open(fn+'.'+suffix, 'r'))
-#
-#  plot(P, fn, site34)
-#
-#  print "Count"
-#  for (site_ids, ct) in count(C).iteritems():
-#    print site_ids, '-->', ct
-#
-#  print "\nCorrelation" # of distance to true position and ellipse area
-#  for (site_ids, r) in correlation(P, C, site34).iteritems():
-#    print site_ids, '--> %0.4f, p-val=%0.4f' % r
-#
-#  print "\nCvg. probability" 
-#  for (site_ids, p) in coverage(P, C).iteritems():
-#    print site_ids, '--> %0.4f' % p
-  
+  (P, C) = pickle.load(open(fn+'.'+suffix, 'r'))
+
+  #plot(P, fn, site34)
+
+  a = sorted(P.keys())
+
+  print "Count"
+  ct = count(C)
+  for site_ids in a:
+    print site_ids, '-->', ct[site_ids]
+
+  print "\nCorrelation" # of distance to true position and ellipse area
+  corr = correlation(P, C, site34)
+  for site_ids in a:
+    if corr[site_ids] == None:
+      print site_ids
+    else:
+      print site_ids, '--> %0.4f, p-val=%0.4f' % corr[site_ids]
+
+  print "\nCvg. probability" 
+  cover = coverage(P, C)
+  for site_ids in a:
+    p = cover[site_ids]
+    if p == None:
+      print site_ids
+    elif p[0] == 0:
+      print site_ids, 0
+    else:
+      print site_ids, '--> %%%.1f %d/%d' % ((float(100*p[0])/p[1],) + p)
+ 
 
